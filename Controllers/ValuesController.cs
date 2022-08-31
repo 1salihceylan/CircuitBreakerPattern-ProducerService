9041bc75bb97924f17acea53b62e6262eec1f468
Salih <salihceylan@windowslive.com>
2022-08-31 11:19:38 +0300

    first commit
    first commit

---
+++ Controllers/ValuesController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProducerService.Controllers
+{
+    [Route("api/producer")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        [HttpGet("error")]
+        public ActionResult<IEnumerable<string>> Error()
+        {
+            throw new Exception("There is an Error");
+        }
+
+        [HttpGet("timeout")]
+        public ActionResult<IEnumerable<string>> TimeOut()
+        {
+            return StatusCode(408);
+        }
+
+        [HttpGet("success")]
+        public ActionResult<IEnumerable<string>> Success()
+        {
+            return Ok("Transaction is Successfull");
+        }
+    }
+}
